92dffc87a876ab67e46802e111e1a14b926375ee
Niles Rowland <niles@reallocalpages.com>
2012-03-02 12:10:51 -0500

    initial work on iso users.

--- isos_app_controller.php
+++ isos_app_controller.php
@@ -1,31 +1,32 @@
 <?php
 class IsosAppController extends AppController {
-
+/* *
 	public function beforeFilter() {
-//debug($this->Auth->password('password'));
-//debug($this->name);
-//debug($this->action);
-//debug($this->layout);
-//debug($scripts_for_layout);
-		$this->_movedHosts();
+		if (Configure::read('debug')) {
+			$this->Email->delivery = 'debug';
+		}
 		$this->_setupLogging();
-		$this->_initializeHosts();
+		$this->Auth->loginAction = array('plugin' => 'isos', 'controller' => 'iso_users', 'action' => 'login');
+		$this->Auth->fields = array('username' => 'email_address', 'password' => 'passwd');
+		$this->Auth->logoutRedirect = '/';
+		$this->AUth->loginError = __('Invalid username / password combination. Please try again.', true);
+		$this->Auth->autoRedirect = false;
+		$this->Auth->authorize = 'controller';
+		$this->Auth->userScope = array(
+			'IsoUser.email_authenticated' => 1
+		);
+		if ($this->Auth->user()) {
+			$this->currentUser = $this->Auth->User();
+			$this->set('currentUser', $currentUser);
+			
+		}
 		$emptyUser = array(
 			'User' => array(
 				'role' => 'ghoti',
-				'business_id' => $this->currentWebsite['Website']['business_id'],
-				'website_id' => $this->currentWebsite['Website']['id'],
 				'name' => 'Nobody',
 				'email_address' => 'nobody@example.com',
 				'password' => sha1('mxyzptlk' . time())
 			)
-		);
-
-		$this->Auth->fields = array('username' => 'email_address', 'password' => 'password');
-		$this->Auth->autoRedirect = false;
-		$this->Auth->userScope = array(
-			'User.deleted' => false,
-			'User.website_id' => $this->currentWebsite['Website']['id']
 		);
 		if ($currentUser = $this->Auth->user()) {
 			$this->currentUser = $currentUser;
@@ -35,7 +36,7 @@ class IsosAppController extends AppController {
 		}
 		$this->_doPrefixes();
 	}
-
+/* */
 }
 
 ?>
--- tests/cases/controllers/iso_users_controller.test.php
+++ tests/cases/controllers/iso_users_controller.test.php
@@ -1,25 +1,28 @@
 <?php
-/* IsoUsers Test cases generated on: 2012-03-02 09:00:55 : 1330696855*/
+/* IsoUsers Test cases generated on: 2012-03-01 16:59:17 : 1330639157*/
 App::import('Controller', 'Isos.IsoUsers');
 
 class TestIsoUsersController extends IsoUsersController {
-	var $autoRender = false;
+	public $autoRender = false;
 
-	function redirect($url, $status = null, $exit = true) {
+	public function redirect($url, $status = null, $exit = true) {
 		$this->redirectUrl = $url;
 	}
 }
 
 class IsoUsersControllerTestCase extends CakeTestCase {
-	function startTest() {
-		$this->IsoUsers =& new TestIsoUsersController();
+	public function startTest() {
+		$this->IsoUsers = new TestIsoUsersController();
 		$this->IsoUsers->constructClasses();
 	}
 
-	function endTest() {
+	public function endTest() {
 		unset($this->IsoUsers);
 		ClassRegistry::flush();
 	}
+	public function testInstance() {
+		$this->assertIsA($this->IsoUsers, 'TestIsoUsersController');
+		debug($this->IsoUsers->Auth);
+	}
 
 }
-?>
